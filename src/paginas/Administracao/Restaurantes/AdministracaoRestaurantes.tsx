45acfea7c11970594661add2c6010d5216a15e5d
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2025-07-01 13:34:01 -0300

    Listando restaurantes

---
+++ src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -0,0 +1,37 @@
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
+import { useEffect, useState } from "react"
+import IRestaurante from "../../../interfaces/IRestaurante"
+import axios from "axios"
+
+const AdministracaoRestaurantes = () => {
+
+    const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
+
+    useEffect(() => {
+        axios.get<IRestaurante[]>('http://127.0.0.1:8000/api/v2/restaurantes/')
+            .then(resposta => setRestaurantes(resposta.data))
+    }, [])
+
+    return (
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>
+                            Nome
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {restaurantes.map(restaurante => <TableRow key={restaurante.id}>
+                        <TableCell>
+                            {restaurante.nome}
+                        </TableCell>
+                    </TableRow>)}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
+}
+
+export default AdministracaoRestaurantes
