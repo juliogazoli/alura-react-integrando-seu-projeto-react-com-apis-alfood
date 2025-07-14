8e60c51e8dbb9a944b0ec486328ffec2b1e33d1a
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2025-07-14 13:00:50 -0300

    Enviando pratos para a API

--- src/paginas/Administracao/Pratos/FormularioPrato.tsx
+++ src/paginas/Administracao/Pratos/FormularioPrato.tsx
@@ -28,6 +28,30 @@ const FormularioPrato = () => {
   const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
     evento.preventDefault()
 
+    const formData = new FormData();
+
+    formData.append('nome', nomePrato)
+    formData.append('descricao', descricao)
+
+    formData.append('tag', tag)
+
+    formData.append('restaurante', restaurante)
+
+    if (imagem) {
+      formData.append('imagem', imagem)
+    }
+
+    http.request({
+      url: 'pratos/',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      },
+      data: formData
+    })
+      .then(() => alert('Prato cadastrado com sucesso!'))
+      .catch(erro => console.log(erro))
+
   }
 
   const selecionarArquivo = (evento: React.ChangeEvent<HTMLInputElement>) => {
@@ -65,7 +89,7 @@ const FormularioPrato = () => {
         <FormControl margin="dense" fullWidth>
           <InputLabel id="select-tag">Tag</InputLabel>
           <Select labelId="select-tag" value={tag} onChange={evento => setTag(evento.target.value)}>
-            {tags.map(tag => <MenuItem key={tag.id} value={tag.id}>
+            {tags.map(tag => <MenuItem key={tag.id} value={tag.value}>
               {tag.value}
             </MenuItem>)}
           </Select>
